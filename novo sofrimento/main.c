90223ebcd7808a3a38a067f908e51523ee50c79d
marcotuiio <96429308+marcotuiio@users.noreply.github.com>
2022-03-24 21:50:06 -0300

    Update main.c

--- novo sofrimento/main.c
+++ novo sofrimento/main.c
@@ -3,67 +3,30 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-//#include "fila.h"
 #include "lista2.h"
-//#include "pilha.h"
-//#include "fila_circular.h"
+//#include "fila_circ.h"
 
 void main() {
-    // printf("INT_MIN = %d\n\n", QUEUE_EMPTY);
-    // printf("--- FILA ---\n");
-    // fila q1, q2, q3;
-
-    // init(&q1);
-    // init(&q2);
-    // init(&q3);
-
-    // enfila(&q1, 34);
-    // enfila(&q2, 75); 
-    // enfila(&q2, 11);
-    // enfila(&q2, 292); 
-    // enfila(&q3, 19);
-    // enfila(&q1, 999);
-    
-    // printFila(&q2);
-    // printFila(&q1);
 
     printf("\n--- LISTA DUPLAMENTE ENCADEADA ---\n");
     Lista lista = criaLista();
-    printf("--- PASSOU DA CRIACAO DA LISTA ---\n");
-
-    for (int i=0; i<25; i++){
+    //printf("--- PASSOU DA CRIACAO DA LISTA ---\n");
+    
+    for (int i=0; i<9; i++){
         insereInicio(lista, i);
     }
+    //printf("--- PASSOU DO INSERE INICIO ---\n");
 
-    printf("--- PASSOU DO INSERE INICIO ---\n");
-
-    int a=3, b=8, c=1;
-    insereDepois(lista, a, b);
-
-    printf("--- PASSOU DO INSERE DEPOIS ---\n");
-
+    int a=3, b=8, c=999;
+    insereDepois(lista, b, c); 
+    //printf("--- PASSOU DO INSERE DEPOIS ---\n");
 
     printList(lista);
 
-    removeCelula(lista, &c);
-    //removeCelula(&head, head);
+    removeCelula(lista, 0);
+    printf("--- PASSOU DO REMOVE CELULA ---\n");
 
     printList(lista);
-
-    // printf("\n--- PILHA ---\n");
-    // pilha s1 = NULL;
-    // pilha s2 = NULL;
-    // pilha s3 = NULL;
-
-    // empilha(&s1, 34); 
-    // empilha(&s2, 75);
-    // empilha(&s2, 11);
-    // empilha(&s2, 292);
-    // empilha(&s3, 19);
-    // empilha(&s1, 999);
-    
-    // printPilha(&s2);
-    // printPilha(&s1);
 
     // printf("\n--- FILA CIRCULAR---\n");
     // fila_circ cq1;
