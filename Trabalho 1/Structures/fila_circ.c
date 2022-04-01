10bf46bbdf76c614fd9828ee8f0ef09a7ad3478d
marcotuiio <96429308+marcotuiio@users.noreply.github.com>
2022-04-01 16:59:12 -0300

    Add files via upload

---
+++ Trabalho 1/Structures/fila_circ.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <limits.h>
+
+#include "fila_circ.h"
+
+#define QUEUE_EMPTY INT_MIN
+
+struct queue{
+    Item value;
+    int head, tail, num_entries, size;
+};
+typedef struct queue fila_circ;
+
+Fila_Circular criaFila(int max_size){
+    //Criando nova fila circular
+    fila_circ *q = calloc(1, sizeof(fila_circ*));
+
+    q->size = max_size;
+    q->value = malloc(sizeof(fila_circ) * q->size);    
+    q->num_entries = 0;
+    q->head = 0;
+    q->tail = 0;
+
+    return q;
+}
+
+bool fila_vazia(Fila_Circular q){
+    fila_circ *fila = (fila_circ*) q;
+
+    return (fila->num_entries == 0);
+}
+
+bool fila_cheia(Fila_Circular q){
+    fila_circ *fila = (fila_circ*) q;
+
+    return (fila->num_entries == fila->size);
+}
+
+void destroi_fila(Fila_Circular q){
+    fila_circ *fila = (fila_circ*) q;
+
+    free(fila->value);
+}
+
+bool enfila_circ(Fila_Circular q, Item x){
+    fila_circ *fila = (fila_circ*) q;
+    //Preencho uma nova celula
+    if (fila_cheia(fila)){
+        printf("Sua fila esta cheia\n");
+        return false;
+    }
+
+    fila->value[fila->tail] = x;
+    fila->num_entries++;
+    fila->tail = (fila->tail + 1) % fila->size;
+    printf("Inserido com sucesso\n");
+    return true;
+}
+
+Item desenfila_circ(Fila_Circular q){
+    fila_circ *fila = (fila_circ*) q;
+    fila_circ *result = NULL;
+
+    if (fila_vazia(fila)){
+        //printf("Sua fila esta vazia\n");
+        return QUEUE_EMPTY;
+    }
+
+    result = fila->value[fila->head];
+    fila->head = (fila->head + 1) % fila->size;
+    fila->num_entries--;
+    //printf("%d - ", result);
+    return result;
+}
+
+void printFila_circ(Fila_Circular q){
+    fila_circ *fila = (fila_circ*) q;
+    
+    int cq;  //Usar 'Item' pois essa fila deve armazar qualquer tipo de valor
+    printf("\n");
+    while ((cq = desenfila_circ(fila)) != QUEUE_EMPTY){
+        printf("q = %d\n", cq);
+    }
+}
---
+++ Trabalho 1/Structures/lista2.h
@@ -0,0 +1,42 @@
+#ifndef LISTA2_H
+#define LISTA2_H
+
+//Lista são estruturas de dados alocadas dinamicamente na qual 
+//escoho a posição onde removerei ou adicionarei um elemento
+
+typedef void* Lista;
+typedef void* Item;
+
+Lista criaLista();
+//Criação de uma lista;
+
+void printList(Lista l);
+//Imprime os elementos a medida que eles são removidos
+
+void insereFim(Lista l, Item n);
+//insere um elemento sempre no final da lista 
+
+void insereInicio(Lista l, Item n);
+//insere um elemento no começo da lista 
+
+void insereDepois(Lista l, Item n, Item x);
+//insere um elemento depois de um determinado elemento anterior
+//n = elemento da lista anterior ao novo
+//x = novo elemento da lista
+
+void removeCelula(Lista l, Item n);
+//remove um elemento e libera a memória
+
+void getFirst(Lista l);
+//exibe o primeiro elemento da lista
+
+void getLast(Lista l);
+//exibe o ultimo elemento da lista
+
+void getLenght(Lista l);
+//exibe a quantidade de elementos na lista
+
+Lista encontraCelula(Lista l, Item n);
+//dada a lista, percorre a mesma buscando o item n desejado 
+
+#endif
